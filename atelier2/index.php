86eba40f43b47216cc991c62e3473039cab51e7b
dceleste35 <145990752+dceleste35@users.noreply.github.com>
2024-12-18 11:15:06 +0100

    Refactor cookie expiration time and token generation in index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -5,8 +5,11 @@ session_start();
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken'])) {
+if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'admin_')) {
     header('Location: page_admin.php');
+    exit();
+} elseif (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'user_')) {
+    header('Location: page_user.php');
     exit();
 }
 
@@ -19,12 +22,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur
     if ($username === 'admin' && $password === 'secret') {
         $token = bin2hex(random_bytes(16));
-        setcookie('authToken', $token, time() + 60, '/', '', true, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes)
+        setcookie('authToken', 'admin_' . $token, time() + 60, '/', '', true, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes)
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } elseif ($username === 'user' && $password === 'utilisateur') {
         $token = bin2hex(random_bytes(16));
-        setcookie('authToken', $token, time() + 60, '/', '', true, true);
+        setcookie('authToken', 'user_' . $token, time() + 60, '/', '', true, true);
         header('Location: page_user.php');
         exit();
     } else {
