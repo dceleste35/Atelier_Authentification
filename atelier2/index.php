9a3a479854af2716ae83eec06bd9edb06dd87276
dceleste35 <145990752+dceleste35@users.noreply.github.com>
2024-12-18 13:05:25 +0100

    Refactor cookie validation and token generation in index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -5,12 +5,14 @@ session_start();
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'admin_')) {
-    header('Location: page_admin.php');
-    exit();
-} elseif (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'user_')) {
-    header('Location: page_user.php');
-    exit();
+if (isset($_SESSION['authToken'])) {
+    if (str_starts_with($_SESSION['authToken'], 'admin_')) {
+        header('Location: page_admin.php');
+        exit();
+    } elseif (str_starts_with($_SESSION['authToken'], 'user_')) {
+        header('Location: page_user.php');
+        exit();
+    }
 }
 
 // Gérer la soumission du formulaire
@@ -22,12 +24,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur
     if ($username === 'admin' && $password === 'secret') {
         $token = bin2hex(random_bytes(16));
-        setcookie('authToken', 'admin_' . $token, time() + 60, '/', '', true, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes)
+        $_SESSION['authToken'] = 'admin_' . $token;
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } elseif ($username === 'user' && $password === 'utilisateur') {
         $token = bin2hex(random_bytes(16));
-        setcookie('authToken', 'user_' . $token, time() + 60, '/', '', true, true);
+        $_SESSION['authToken'] = 'user_' . $token;
         header('Location: page_user.php');
         exit();
     } else {
